96425e9676b618bda3ab8e05902ce7bd0ffad56f
Shahar <shaharariel95@gmail.com>
2024-05-04 17:12:38 +0300

    logger Class

---
+++ proj_logger.py
@@ -0,0 +1,13 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+
+def get_logger():
+    return logger
