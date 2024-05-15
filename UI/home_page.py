79b31b0945a5785a5a8cc83d9c9bcfdfedabc124
Shahar <shaharariel95@gmail.com>
2024-05-15 23:34:19 +0300

    working homepage + identify page + moving beween them

--- UI/home_page.py
+++ UI/home_page.py
@@ -2,19 +2,34 @@ import tkinter as tk
 from PIL import ImageTk, Image
 from identify_page import IdentifyPage
 
+
+##TODO: delete this, its for testig
+import mediapipe as mp
+import tensorflow as tf
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 # main HomePage Frame
 class HomePage(tk.Frame):      
     def __init__(self, parent, BG_COLOR, logo_img, name_break_down_img, home_page_boy_img, identify_img, word_identify_img, sign_a_word_img):
         super().__init__(parent, bg=BG_COLOR)
         self.root = parent
         self.top_frame = TopHomeFrame(self,self.root, BG_COLOR, logo_img)
-        self.middle_frame = MiddleHomeFrame(self,self.root, self.homePage_forget, BG_COLOR, name_break_down_img, home_page_boy_img, identify_img, word_identify_img, sign_a_word_img)
+        self.middle_frame = MiddleHomeFrame(self,self.root, self.homePage_forget, self.homePage_show, BG_COLOR, name_break_down_img, home_page_boy_img, identify_img, word_identify_img, sign_a_word_img)
         # self.bottom_frame = BottomHomeFrame(self, BG_COLOR)
         self.pack(expand=True, fill='both')
 
     def homePage_forget(self):
         self.top_frame.pack_forget()
         self.middle_frame.pack_forget()
+        self.pack_forget()
+
+    def homePage_show(self):
+        self.top_frame.pack(side='top', fill='x', pady=2)
+        self.middle_frame.pack()
+        self.pack(expand=True, fill='both')
 
 # end main HomePage Frame
 
@@ -34,17 +49,17 @@ class TopHomeFrame(tk.Frame):
 
 # middle frame
 class MiddleHomeFrame(tk.Frame):
-    def __init__(self, parent, root, homepage_foget, BG_COLOR, het_img, boy_img, identify_img, left_bottom_img, right_bottom_img):
+    def __init__(self, parent, root, homepage_foget,homePage_show,  BG_COLOR, het_img, boy_img, identify_img, left_bottom_img, right_bottom_img):
         super().__init__(parent, bg=BG_COLOR)  # BG_COLOR
         self.root = root
         self.pack()
-        self.create_widgets(BG_COLOR, self.root, homepage_foget, het_img, boy_img, identify_img, left_bottom_img, right_bottom_img)
+        self.create_widgets(BG_COLOR, self.root, homepage_foget, homePage_show,  het_img, boy_img, identify_img, left_bottom_img, right_bottom_img)
 
-    def create_widgets(self, BG_COLOR, root, homepage_foget, het_img, boy_img, identify_img, left_bottom_img, right_bottom_img):
+    def create_widgets(self, BG_COLOR, root, homepage_foget, homePage_show,  het_img, boy_img, identify_img, left_bottom_img, right_bottom_img):
         self.left_frame = LeftHomeFrame(self, root, homepage_foget, BG_COLOR, het_img, left_bottom_img)
         boy_label = tk.Label(self, image=boy_img, bg=BG_COLOR)  # BG_COLOR
         boy_label.pack(side="left")
-        self.right_frame = RightHomeFrame(self, root, homepage_foget,  BG_COLOR, identify_img, right_bottom_img)
+        self.right_frame = RightHomeFrame(self, root, homepage_foget, homePage_show,  BG_COLOR, identify_img, right_bottom_img)
 
 # end middle frame
 
@@ -66,21 +81,33 @@ class LeftHomeFrame(tk.Frame):
 
 # right frame
 class RightHomeFrame(tk.Frame):
-    def __init__(self, parent, root, homepage_foget, BG_COLOR, identify_img, right_bottom_img):
+    def __init__(self, parent, root, homepage_foget, homePage_show,  BG_COLOR, identify_img, right_bottom_img):
         super().__init__(parent, bg=BG_COLOR)
         self.root = root
         self.homepage_foget = homepage_foget
+        self.homePage_show = homePage_show
         self.pack(side="left", padx=10)
         self.identify_page = None
+        # Create a configuration dictionary
+        self.config = {
+            "BG_COLOR": "#FEE4FC",
+            "boy_img":  identify_boy_bubble,
+            "back_img": back_img,
+            "meet_the_letter": meet_the_letter,
+            "close_camera": lambda: None,  # Placeholder for close_camera function
+            "show_home_frame": lambda: None,  # Placeholder for show_home_frame function
+            "video_label": tk.Label() , # Placeholder for video_label
+            "homePage_show": self.homePage_show
+        }
 
         self.create_widgets( BG_COLOR, identify_img, right_bottom_img)
     
     ## TODO: add a root var that is passed down to the buttons
     def show_identify_page(self):
         print(f'in show_identify_page')
-        self.identify_page = IdentifyPage(self.root)
+        self.identify_page = IdentifyPage(self.root, self.config)
         self.homepage_foget()
-        self.identify_page.show
+        # self.identify_page.show
 
     def create_widgets(self, BG_COLOR, identify_img, right_bottom_img):
         right_button = tk.Button(self, image=identify_img, command=lambda: [self.show_identify_page()], bg=BG_COLOR, borderwidth=0)
@@ -126,11 +153,12 @@ name_break_down_img = ImageTk.PhotoImage(Image.open("new_assets/NameBreakDown.pn
 identify_img = ImageTk.PhotoImage(Image.open("new_assets/IdentifyPage.png").resize((250, 250), Image.LANCZOS))
 sign_a_word_img = ImageTk.PhotoImage(Image.open("new_assets/SignAWord.png").resize((250, 250), Image.LANCZOS))
 
-back_img = ImageTk.PhotoImage(Image.open("assets/back.png").resize((124, 67), Image.LANCZOS))
+back_img = ImageTk.PhotoImage(Image.open("new_assets/back.png").resize((250, 62), Image.LANCZOS))
 submit_img = ImageTk.PhotoImage(Image.open("assets/submit.png").resize((124, 67), Image.LANCZOS))
 next_img = ImageTk.PhotoImage(Image.open("assets/next.png").resize((154, 68), Image.LANCZOS))
 
-
+identify_boy_bubble = ImageTk.PhotoImage(Image.open("new_assets/boy_bubble.png").resize((400, 500), Image.LANCZOS))
+meet_the_letter = ImageTk.PhotoImage(Image.open("new_assets/meet_the_letter.png").resize((250, 62), Image.LANCZOS))
 
 home_page = HomePage(root, BG_COLOR, logo_img, name_break_down_img, home_page_boy_img, identify_img, word_identify_img, sign_a_word_img)
 
--- UI/identify_page.py
+++ UI/identify_page.py
@@ -8,143 +8,129 @@ from proj_camera import proj_camera
 from proj_logger import get_logger
 
 
+
 # main identify Frame
 class IdentifyPage(tk.Frame):
-    def __init__(self, parent, BG_COLOR, boy_img, back_img, meet_the_letter, close_camera, show_home_frame,
-                 video_label):
-        super().__init__(parent, bg=BG_COLOR)
+    def __init__(self, parent, config):
+        super().__init__(parent, bg=config["BG_COLOR"])
         self.logger = get_logger()
         self.logger.info(f'starting init of IdentifyPage')
+        self.config = config
         self.cap = proj_camera()
-        self.top_frame_identify = TopIdentifyFrame(self, BG_COLOR, back_img)
-        self.middle_frame_identify = MiddleIdentifyFrame(self, self.cap, boy_img, meet_the_letter)
-        # self.bottom_frame_identify = bottom_identify_frame(self, back_img)
+        self.config["cap"] = self.cap
+        self.top_frame_identify = TopIdentifyFrame(self, self.config)
+        self.middle_frame_identify = MiddleIdentifyFrame(self, self.config)
+        self.pack(expand=True, fill='both')
 
     def close_frame(self):
-        self.cap.close_camera()
+        
+        self.config["cap"].close_camera()
         self.pack_forget()
-# end main identify frame
 
 # top frame
 class TopIdentifyFrame(tk.Frame):
-    def __init__(self, parent, BG_COLOR, back_img):
-        super().__init__(parent, bg=BG_COLOR)
+    def __init__(self, parent, config):
+        super().__init__(parent, bg=config["BG_COLOR"])
+        self.parent = parent
+        self.config = config
         self.pack(side='top', fill='x')
-        self.create_widgets(back_img)
+        self.create_widgets()
 
-    def create_widgets(self, back_img):
-        back_button = tk.Button(self, image=back_img, bg=BG_COLOR, borderwidth=0,
-                                highlightbackground=BG_COLOR, highlightcolor=BG_COLOR, highlightthickness=0)
+    def back_to_homepage(self):
+        self.parent.close_frame()
+        self.config["homePage_show"]()
+
+    def create_widgets(self):
+        back_button = tk.Button(self, image=self.config["back_img"], bg=self.config["BG_COLOR"], borderwidth=0, command= self.back_to_homepage,
+                                highlightbackground=self.config["BG_COLOR"], highlightcolor=self.config["BG_COLOR"], highlightthickness=0)
         back_button.pack(side='right', padx=30, pady=10)
-
-
-# end top frame
 
 # middle main frame
 class MiddleIdentifyFrame(tk.Frame):
-    def __init__(self, parent, cap, boy_img, meet_the_letter):
-        super().__init__(parent, bg=BG_COLOR)
+    def __init__(self, parent, config):
+        super().__init__(parent, bg=config["BG_COLOR"])
         self.pack(side='top', expand=True, fill='both', pady=15)
-        self.cap = cap
-        self.create_widgets(boy_img, self.cap, meet_the_letter)
+        self.config = config
+        # self.cap = proj_camera()
+        self.create_widgets()
 
-    def create_widgets(self, boy_img, cap, meet_the_letter):
-        self.middle_left = middle_left_identify_frame(self, boy_img)
-        self.middle_right = middle_right_identify_frame(self, cap, meet_the_letter)
-
-
-# end middle main frame
+    def create_widgets(self):
+        self.middle_left = middle_left_identify_frame(self, self.config)
+        self.middle_right = middle_right_identify_frame(self, self.config)
 
 # middle left frame
 class middle_left_identify_frame(tk.Frame):
-    def __init__(self, parent, boy_img):
-        super().__init__(parent, bg=BG_COLOR)
+    def __init__(self, parent, config):
+        super().__init__(parent, bg=config["BG_COLOR"])
         self.pack(side='left', fill='both')
-        self.create_widgets(boy_img)
+        self.config = config
+        self.create_widgets()
 
-    def create_widgets(self, boy_img):
-        identify_boy_label = tk.Label(self, image=boy_img, bg=BG_COLOR)
+    def create_widgets(self):
+        identify_boy_label = tk.Label(self, image=self.config["boy_img"], bg=self.config["BG_COLOR"])
         identify_boy_label.pack(padx=30, expand=True)
-
-
-# end middle left frame
 
 # middle right frame
 class middle_right_identify_frame(tk.Frame):
-    def __init__(self, parent, cap, meet_the_letter):
-        super().__init__(parent, bg=BG_COLOR)
+    def __init__(self, parent, config):
+        super().__init__(parent, bg=config["BG_COLOR"])
         self.pack(expand=True, fill='both')
-        self.cap = cap
-        self.create_widgets(meet_the_letter)
+        self.config = config
+        #TODO: close camera
+        # self.cap = proj_camera()
+        self.create_widgets()
 
-    def create_widgets(self, meet_the_letter):
+    def create_widgets(self):
         # video frame
-        self.identify_video_label = tk.Label(self, bg=BG_COLOR)
+        self.identify_video_label = tk.Label(self, bg=self.config["BG_COLOR"])
         self.identify_video_label.pack(side='top', fill='both', expand=True)
 
         # prediction frame
-        self.prediction_frame = tk.Frame(self, bg=BG_COLOR, padx=10, pady=5)
+        self.prediction_frame = tk.Frame(self, bg=self.config["BG_COLOR"], padx=10, pady=5)
         self.prediction_frame.pack(side="bottom", pady=0, padx=20, fill='x')
-        self.prediction_label = tk.Label(self.prediction_frame, text="ז", bg=BG_COLOR, font=("Calibri", 80, 'bold'))
+        self.prediction_label = tk.Label(self.prediction_frame, text="ז", bg=self.config["BG_COLOR"], font=("Calibri", 80, 'bold'))
         self.empty_frame = tk.Label(self.prediction_frame, width=int(self.prediction_frame.winfo_width() / 10),
-                                    bg=BG_COLOR)
+                                    bg=self.config["BG_COLOR"])
         self.empty_frame.pack(side='left', expand=True, fill='x')
         self.prediction_label.pack(side="left", pady=0)
-        self.prediction_label_header = tk.Label(self.prediction_frame, image=meet_the_letter, bg=BG_COLOR,
+        self.prediction_label_header = tk.Label(self.prediction_frame, image=self.config["meet_the_letter"], bg=self.config["BG_COLOR"],
                                                 font=("Calibri", 40))
         self.prediction_label_header.pack(side='left', expand=True)
-        self.cap.start_camera(self.identify_video_label, self.prediction_label)
-
-
-# end middle right frame
+        self.config["cap"].start_camera(self.identify_video_label, self.prediction_label)
 
 # bottom frame (not in use )
 class bottom_identify_frame(tk.Frame):
-    def __init__(self, parent, back_img):
-        super().__init__(parent, bg=BG_COLOR)
+    def __init__(self, parent, config):
+        super().__init__(parent, bg=config["BG_COLOR"])
         self.pack(side='bottom', expand=True, fill='x')
+        self.create_widgets(config)
 
-        self.create_widgets(back_img)
-
-    def create_widgets(self, back_img):
-        back_button = tk.Button(self, image=back_img, bg=BG_COLOR, borderwidth=0,
-                                highlightbackground=BG_COLOR, highlightcolor=BG_COLOR, highlightthickness=0)
+    def create_widgets(self, config):
+        back_button = tk.Button(self, image=config["back_img"], bg=config["BG_COLOR"], borderwidth=0,
+                                highlightbackground=config["BG_COLOR"], highlightcolor=config["BG_COLOR"], highlightthickness=0)
         back_button.pack(side='left', padx=30, pady=10)
 
-
-# end bottom frame ( not in use )
-
-# ## temp empty root for testing
+# Main script
 # root = tk.Tk()
 # root.title("SignMyName")
-# root.configure(bg='black')  # Set the background color
+# root.configure(bg='black')
 # root.minsize(1200, 720)
 # root.iconbitmap("assets/icon.ico")
-BG_COLOR = "#FEE4FC"
-
-# # in place of the live video
-# placeholder_img = ImageTk.PhotoImage(Image.open("placeholder.png").resize((640, 480), Image.LANCZOS))
-
-# # input images for class
-# boy_bubble_img = ImageTk.PhotoImage(Image.open("new_assets/boy_bubble.png").resize((400, 500), Image.LANCZOS))
-# back_button = ImageTk.PhotoImage(Image.open("new_assets/back.png").resize((250, 62), Image.LANCZOS))
-# meet_the_letter = ImageTk.PhotoImage(Image.open("new_assets/meet_the_letter.png").resize((369, 80), Image.LANCZOS))
-
-# ## need to be in use!
-# video_label = tk.Label()
 
 
-# # place holder for proj_camera
-# def close_camera():
-#     pass
-
-
-# def show_home_frame():
-#     pass
+# Create a configuration dictionary
+# config = {
+#     "BG_COLOR": "#FEE4FC",
+#     "boy_img":  ImageTk.PhotoImage(Image.open("new_assets/boy_bubble.png").resize((400, 500), Image.LANCZOS)),
+#     "back_img": ImageTk.PhotoImage(Image.open("new_assets/back.png").resize((250, 62), Image.LANCZOS)),
+#     "meet_the_letter": ImageTk.PhotoImage(Image.open("new_assets/meet_the_letter.png").resize((369, 80), Image.LANCZOS)),
+#     "close_camera": lambda: None,  # Placeholder for close_camera function
+#     "show_home_frame": lambda: None,  # Placeholder for show_home_frame function
+#     "video_label": tk.Label()  # Placeholder for video_label
+# }
 
 
 # # Create an instance of the IdentifyPage class
-# identify_page = IdentifyPage(root, BG_COLOR, boy_bubble_img, back_button, meet_the_letter, close_camera,
-#                              show_home_frame, video_label)
+# identify_page = IdentifyPage(root, config)
 
 # root.mainloop()
