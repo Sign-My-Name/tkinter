0163459def63efce4b7ed386149605d7abb8cc09
Shahar <shaharariel95@gmail.com>
2024-08-20 16:30:24 +0300

    added comments to proj_logger.py

--- proj_logger.py
+++ proj_logger.py
@@ -1,13 +1,20 @@
 import logging
 
+"""
+configuring the logger.
+"""
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
 
 def get_logger():
+    """
+    Returns the configured logger instance.
+    """
     return logger
